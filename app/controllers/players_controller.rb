c10229c10c59dc42e2ac14ec2d34e2591badb4d1
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-09-27 20:09:46 -0400

    Add player index controller method

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,6 +1,10 @@
 class PlayersController < ApplicationController
     def index
-
+        players = Player.all
+        options = {
+            include: [:team]
+        }
+        render json: PlayerSerializer.new(players, options)
     end
 
     def show
