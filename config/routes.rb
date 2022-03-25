bedf4d6e6370887ac8992a19bac2644fa2be490a
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2022-03-25 12:54:32 -0500

    Update root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   
-  root "teams#index"
+  root "players#index"
 
-  
+
   resources :players
   resources :teams
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
